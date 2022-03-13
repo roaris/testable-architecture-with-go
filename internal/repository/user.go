3497fbaf27c6d5aab745b4f9006f96002fb33366
roaris <one0803kyopro@gmail.com>
2022-03-13 16:56:04 +0900

    repositoryの切り出し

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,6 +2,9 @@ package repository
 
 import (
 	"context"
+	"database/sql"
+
+	"github.dena.jp/swet/go-sampleapi/internal/apierr"
 
 	"github.com/jmoiron/sqlx"
 
@@ -16,10 +19,25 @@ func NewUser() *User {
 
 func (u *User) FindByEmail(ctx context.Context, queryer sqlx.QueryerContext, email string) (*model.User, error) {
 	// TODO methodを実装する
-	panic("implement me")
+	var m model.User
+	if err := sqlx.GetContext(ctx, queryer, &m, "select * from users where email = ?", email); err == sql.ErrNoRows {
+		return nil, apierr.ErrUserNotExists
+	} else if err != nil {
+		return nil, err
+	}
+	return &m, nil
 }
 
 func (u *User) Create(ctx context.Context, execer sqlx.ExecerContext, m *model.User) error {
 	// TODO methodを実装する
-	panic("implement me")
+	result, err := execer.ExecContext(ctx, "insert into users(first_name, last_name, email, password_hash) values(?, ?, ?, ?)", m.FirstName, m.LastName, m.Email, m.PasswordHash)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	m.ID = int(id)
+	return nil
 }
