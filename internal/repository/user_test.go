0e100affc67e70b8725bd1cee55ffe613f4d8461
roaris <one0803kyopro@gmail.com>
2022-03-13 18:34:24 +0900

    repositoryのテスト

--- internal/repository/user_test.go
+++ internal/repository/user_test.go
@@ -1,12 +1,14 @@
 package repository
 
 import (
+	"context"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 
 	"github.dena.jp/swet/go-sampleapi/internal/config"
+	"github.dena.jp/swet/go-sampleapi/internal/model"
 )
 
 func TestUser_FindByEmail(t *testing.T) {
@@ -19,19 +21,35 @@ func TestUser_FindByEmail(t *testing.T) {
 		db.Close()
 	}()
 
-	/*
-		expect := &model.User{
-			Email: "test@example.com",
-			PasswordHash: "dummy",
-			FirstName: "dummy",
-			LastName: "dummy",
-		}
-	*/
+	expect := &model.User{
+		Email:        "test@example.com",
+		PasswordHash: "dummy",
+		FirstName:    "dummy",
+		LastName:     "dummy",
+	}
 
 	// TODO: Test Fixtureの投入
+	db.MustExec("insert into users(first_name, last_name, email, password_hash) values(?, ?, ?, ?)",
+		expect.FirstName, expect.LastName, expect.Email, expect.PasswordHash)
 
 	// TODO: FindByEmailを呼び出す
+	repo := NewUser()
+	u, err := repo.FindByEmail(context.Background(), db, "test@example.com")
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	// TODO: GetしたModelとexpectを比較する
-
+	if u.Email != expect.Email {
+		t.Errorf("email must be %s but %s", expect.Email, u.Email)
+	}
+	if u.PasswordHash != expect.PasswordHash {
+		t.Errorf("password_hash must be %s but %s", expect.PasswordHash, u.PasswordHash)
+	}
+	if u.FirstName != expect.FirstName {
+		t.Errorf("first_name must be %s but %s", expect.FirstName, u.FirstName)
+	}
+	if u.LastName != expect.LastName {
+		t.Errorf("last_name must be %s but %s", expect.LastName, u.LastName)
+	}
 }
